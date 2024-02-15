df77ce0a5bce71faf3eb0e2a17710a2e96286ad1
DeonGeorge44 <ddeongeorge44@gmail.com>
2024-02-16 02:56:55 +0530

    access user input

---
+++ 7-Getting-Input-From-User/app.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+const msg = chalk.red.inverse.bold("ERROR!");
+const msgSuccess = chalk.green.bold.inverse("SUCCESS!!!....");
+console.log(msg);
+console.log(msgSuccess);
+
